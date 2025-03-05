f952dd0fdf29cedf6a49960a88eb5a50a52876cb
Ashish K. Shrestha <ashish.ashishshrestha@gmail.com>
2025-03-05 13:21:27 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:real_estate_management_platform/app.dart';
 import 'package:real_estate_management_platform/app/di/di.dart';
 
@@ -6,5 +7,9 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initDependencies();
   // Ensure dependencies are initialized
-  runApp(const App());
+  runApp(
+    ProviderScope(  // ✅ Wrap the app in ProviderScope
+      child: App(),
+    ),
+  );
 }
